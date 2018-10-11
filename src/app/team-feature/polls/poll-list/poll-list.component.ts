c1416b10ff6c55ffed61d0674a7c9b580b490ab8
Aublanc <vincent.aublanc@gmail.com>
2018-10-11 10:19:06 +0200

    ajout d'une table pour l'affichage des options

--- src/app/team-feature/polls/poll-list/poll-list.component.ts
+++ src/app/team-feature/polls/poll-list/poll-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Poll } from '../shared/poll';
 import { Option } from '../../options/shared/option';
 import { NatureEnum } from 'src/app/shared/nature-enum';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-poll-list',
@@ -13,6 +14,8 @@ export class PollListComponent implements OnInit {
 
   @Input() polls: Poll[];
   currentOptions: Option[];
+  displayedColumns = ['select', 'name'];
+  selection = new SelectionModel<Option>(true, []);
 
   constructor(
     private optionService: OptionService
@@ -23,11 +26,28 @@ export class PollListComponent implements OnInit {
 
   getOptions(poll: Poll): void {
     this.optionService.getOptions(poll).subscribe(
-      optionsReturned => this.currentOptions = optionsReturned);
+      optionsReturned => {
+        this.currentOptions = optionsReturned;
+        this.currentOptions.forEach(row => this.selection.select(row));
+      });
   }
 
   emptyOptions(): void {
     this.currentOptions = [];
+    this.selection.clear();
   }
+
+    // method to know if all the checkboxes are selected, in order to know whether the master checkbox is checked or not
+    isAllSelected() {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.currentOptions.length;
+      return numSelected === numRows;
+    }
+
+    // if the master checkbox is checked, then we check the other checkboxes
+    // if the master checkbox is unchecked, then we uncheck the other checkboxes
+    masterToggle() {
+      this.isAllSelected() ? this.selection.clear() : this.currentOptions.forEach(row => this.selection.select(row));
+    }
 
 }
